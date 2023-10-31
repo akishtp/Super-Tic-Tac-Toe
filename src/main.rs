5e2f6168eccc6e63d25e55fee9ffe23c7783ff5e
Akish T P <akishtp@Akishs-Air.router.local>
2023-10-31 22:22:39 +0530

    table finish

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,44 @@
+use std::io;
+
+const TABLE:[[char;9];9] = [
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+    [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+];
+
 fn main() {
     draw_table();
+    println!("{}", TABLE[1][1]);
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 }
 
 fn draw_table(){
-    println!("---------------------------");
-    println!("| |x|o|x| |x|o|x| |x|o|x| |");
-    println!("| |o|x|o| |o|x|o| |o|x|o| |");
-    println!("| |x|o|x| |x|o|x| |x|o|x| |");
-    println!("---------------------------");
-    println!("| |x|o|x| |x|o|x| |x|o|x| |");
-    println!("| |o|x|o| |o|x|o| |o|x|o| |");
-    println!("| |x|o|x| |x|o|x| |x|o|x| |");
-    println!("---------------------------");
-    println!("| |x|o|x| |x|o|x| |x|o|x| |");
-    println!("| |o|x|o| |o|x|o| |o|x|o| |");
-    println!("| |x|o|x| |x|o|x| |x|o|x| |");
-    println!("---------------------------");
+//    clearscreen::clear().expect("failed to clear screen");
+    println!("┌─┬─────┬─┬─────┬─┬─────┬─┐");
+    println!("│ │{}╎{}╎{}│ │{}╎{}╎{}│ │{}╎{}╎{}│ │");
+    println!("│ │{}╎{}╎{}│ │{}╎{}╎{}│ │{}╎{}╎{}│ │");
+    println!("│ │{}╎{}╎{}│ │{}╎{}╎{}│ │{}╎{}╎{}│ │");
+    println!("├─┼─────┼─┼─────┼─┼─────┼─┤");
+    println!("│ │x╎o╎x│ │x╎o╎x│ │x╎o╎x│ │");
+    println!("│ │o╎x╎o│ │o╎x╎o│ │o╎x╎o│ │");
+    println!("│ │x╎o╎x│ │x╎o╎x│ │x╎o╎x│ │");
+    println!("├─┼─────┼─┼─────┼─┼─────┼─┤");
+    println!("│ │x╎o╎x│ │x╎o╎x│ │x╎o╎x│ │");
+    println!("│ │o╎x╎o│ │o╎x╎o│ │o╎x╎o│ │");
+    println!("│ │x╎o╎x│ │x╎o╎x│ │x╎o╎x│ │");
+    println!("└─┴─────┴─┴─────┴─┴─────┴─┘");
+}
+
+fn player(){
+    
 }
