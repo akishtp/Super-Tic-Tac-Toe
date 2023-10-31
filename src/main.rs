5a8561c90f4160caa357c7d3bdf9029365095666
Akish T P <akishtp@Akishs-Air.router.local>
2023-10-31 13:04:35 +0530

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Init");
+    draw_table();
+}
+
+fn draw_table(){
+    println!("---------------------------");
+    println!("| |x|o|x| |x|o|x| |x|o|x| |");
+    println!("| |o|x|o| |o|x|o| |o|x|o| |");
+    println!("| |x|o|x| |x|o|x| |x|o|x| |");
+    println!("---------------------------");
+    println!("| |x|o|x| |x|o|x| |x|o|x| |");
+    println!("| |o|x|o| |o|x|o| |o|x|o| |");
+    println!("| |x|o|x| |x|o|x| |x|o|x| |");
+    println!("---------------------------");
+    println!("| |x|o|x| |x|o|x| |x|o|x| |");
+    println!("| |o|x|o| |o|x|o| |o|x|o| |");
+    println!("| |x|o|x| |x|o|x| |x|o|x| |");
+    println!("---------------------------");
 }
